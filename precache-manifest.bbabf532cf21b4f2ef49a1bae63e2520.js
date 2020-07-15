71ef8acecd183417baba02c8d6bd43040b8b8ace
Norberto Alonso <alonsonorberto@gmail.com>
2020-07-15 22:22:33 +0100

    Updates

---
+++ precache-manifest.bbabf532cf21b4f2ef49a1bae63e2520.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8c6c86ad1fab395eaa173fb9aac31b",
+    "url": "/pin-pad/index.html"
+  },
+  {
+    "revision": "b3d1ed4ba04691b2e321",
+    "url": "/pin-pad/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "77df7950605cd87c8b1e",
+    "url": "/pin-pad/static/css/main.8e109c2b.chunk.css"
+  },
+  {
+    "revision": "b3d1ed4ba04691b2e321",
+    "url": "/pin-pad/static/js/2.aa791021.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pin-pad/static/js/2.aa791021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77df7950605cd87c8b1e",
+    "url": "/pin-pad/static/js/main.d523dbca.chunk.js"
+  },
+  {
+    "revision": "afbc3fadac4279b289dd",
+    "url": "/pin-pad/static/js/runtime-main.6c9f1fcc.js"
+  },
+  {
+    "revision": "93787297a5a3a02c204ead1b86165ab0",
+    "url": "/pin-pad/static/media/DS-DIGIB.93787297.otf"
+  },
+  {
+    "revision": "94c46079bdde06f768d3b7800fa3f8bc",
+    "url": "/pin-pad/static/media/DS-DIGIB.94c46079.woff"
+  },
+  {
+    "revision": "e45173f67081293da6cf307afb655fd2",
+    "url": "/pin-pad/static/media/bg.e45173f6.jpg"
+  }
+]);
